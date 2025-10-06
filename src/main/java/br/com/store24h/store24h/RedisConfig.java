cc4504593a3feb826a7b3242476174422da59734
zearpro <nzl91@pm.me>
2025-10-06 09:58:27 -0300

    Valkey TLS config on main, restore docker-compose.prod.yml, RabbitMQ env wiring; deploy.sh defaults to main

--- src/main/java/br/com/store24h/store24h/RedisConfig.java
+++ src/main/java/br/com/store24h/store24h/RedisConfig.java
@@ -4,7 +4,6 @@
  * Could not load the following classes:
  *  org.springframework.context.annotation.Bean
  *  org.springframework.context.annotation.Configuration
- *  org.springframework.data.redis.connection.RedisClusterConfiguration
  *  org.springframework.data.redis.connection.RedisConnectionFactory
  *  org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration
  *  org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
@@ -16,14 +15,11 @@
  */
 package br.com.store24h.store24h;
 
-import br.com.store24h.store24h.Utils;
-import br.com.store24h.store24h.VersionEnum;
-import java.util.Arrays;
-import java.util.List;
+import java.time.Duration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisClusterConfiguration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -34,41 +30,54 @@ import org.springframework.session.data.redis.config.ConfigureRedisAction;
 
 @Configuration
 public class RedisConfig {
-    @Bean
-    public static ConfigureRedisAction configureRedisAction() {
-        return ConfigureRedisAction.NO_OP;
-    }
+	@Bean
+	public static ConfigureRedisAction configureRedisAction() {
+		return ConfigureRedisAction.NO_OP;
+	}
 
-    @Bean
-    public LettuceConnectionFactory redisConnectionFactory() {
-        // Get Redis configuration from environment variables
-        String redisHost = System.getenv("REDIS_HOST");
-        String redisPort = System.getenv("REDIS_PORT");
-        String redisPassword = System.getenv("REDIS_PASSWORD");
-        
-        // Default to DragonflyDB container configuration
-        if (redisHost == null) redisHost = "dragonfly";
-        if (redisPort == null) redisPort = "6379";
-        if (redisPassword == null) redisPassword = "";
-        
-        int port = Integer.parseInt(redisPort);
-        
-        // Minimal Redis connection logging - only on errors
-        
-        // Create simple connection factory for Redis container
-        LettuceConnectionFactory factory = new LettuceConnectionFactory(redisHost, port);
-        factory.setPassword(redisPassword);
-        factory.setValidateConnection(true);
-        
-        return factory;
-    }
+	@Bean
+	public LettuceConnectionFactory redisConnectionFactory() {
+		String redisHost = System.getenv("REDIS_HOST");
+		String redisPort = System.getenv("REDIS_PORT");
+		String redisPassword = System.getenv("REDIS_PASSWORD");
+		String redisUsername = System.getenv("REDIS_USERNAME");
+		String redisSsl = System.getenv("REDIS_SSL");
 
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory connectionFactory) {
-        RedisTemplate template = new RedisTemplate();
-        template.setConnectionFactory((RedisConnectionFactory)connectionFactory);
-        template.setKeySerializer((RedisSerializer)new StringRedisSerializer());
-        template.setValueSerializer((RedisSerializer)new GenericToStringSerializer(Object.class));
-        return template;
-    }
+		if (redisHost == null) redisHost = "dragonfly";
+		if (redisPort == null) redisPort = "6379";
+		if (redisPassword == null) redisPassword = "";
+
+		int port = Integer.parseInt(redisPort);
+		boolean useSsl = redisSsl != null ? Boolean.parseBoolean(redisSsl) : false;
+
+		RedisStandaloneConfiguration standalone = new RedisStandaloneConfiguration(redisHost, port);
+		if (redisUsername != null && !redisUsername.isEmpty()) {
+			standalone.setUsername(redisUsername);
+		}
+		if (redisPassword != null && !redisPassword.isEmpty()) {
+			standalone.setPassword(RedisPassword.of(redisPassword));
+		}
+
+		LettuceClientConfiguration.LettuceClientConfigurationBuilder builder = LettuceClientConfiguration.builder()
+				.commandTimeout(Duration.ofSeconds(30));
+		if (useSsl) {
+			builder.useSsl();
+		}
+
+		LettuceClientConfiguration clientConfig = builder.build();
+		LettuceConnectionFactory factory = new LettuceConnectionFactory(standalone, clientConfig);
+		factory.setValidateConnection(true);
+		return factory;
+	}
+
+	@Bean
+	public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory connectionFactory) {
+		RedisTemplate<String, Object> template = new RedisTemplate<>();
+		template.setConnectionFactory(connectionFactory);
+		RedisSerializer<String> keySerializer = new StringRedisSerializer();
+		RedisSerializer<Object> valueSerializer = new GenericToStringSerializer<>(Object.class);
+		template.setKeySerializer(keySerializer);
+		template.setValueSerializer(valueSerializer);
+		return template;
+	}
 }
